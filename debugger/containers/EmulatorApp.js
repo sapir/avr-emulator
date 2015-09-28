603b44760a644835a708f71f33d142843d14fa2c
Chris Conley <pair+chris@realscout.com>
2015-09-28 09:46:26 -0700

    start changing over to hook up to rails

--- debugger/containers/EmulatorApp.js
+++ debugger/containers/EmulatorApp.js
@@ -12,43 +12,63 @@ export default class EmulatorApp {
     return (
       <Connector>
         {({ emulator, programRunner, dispatch }) =>
-          <div className="row">
-            <div className="col-sm-12">
-              <h1 className='main-title'>Atmel AVR 8-bit Emulator in React and Rust</h1>
-            </div>
-            <div className="col-sm-4">
+          <div className="rs-ui">
+            <div className="action-item-header">
+              <div className="row">
+                <div className="col-1-2">
+                  <h5 className="h5">
+                    Messages &amp; Showing Requests (1)
+                  </h5>
+                </div>
+                <div className="col-1-2 text-right">
+                  <div className="sorting-container">
+                    <button className="latest-action-items text-button">Latest</button>
 
-              <div className='program panel'>
-                <h2>Program</h2>
-                <DebuggerButtons programRunner={programRunner}
-                  {...bindActionCreators(DebuggerActions, dispatch)} />
-                <AssemblyProgram instructions={emulator.instructions} programPointer={emulator.program_pointer} />
+                    <button className="archived-action-items text-button grey">Completed</button>
+                  </div>
+                </div>
               </div>
+            </div>
 
-              <div className='panel'>
-                <h2>About</h2>
-                <p>
-                  Inspired by&nbsp;
-                  <a href="http://sockpuppet.org/blog/2015/07/13/starfighter/">
-                    Starfighter's Summer 2015 announcement
-                  </a>,
-                  we thought that implementing our own&nbsp;
-                  <a href="http://www.atmel.com/images/atmel-0856-avr-instruction-set-manual.pdf">
-                    Atmel AVR 8-bit
-                  </a>
-                  &nbsp;emulator would be a pretty fun problem to tackle.
-                  Built with React.js, Redux.js, Rust and Docker
-                  during our last&nbsp;
-                  <a href="https://twitter.com/chrisconley/status/618830194971774976">
-                    biweekly hack day
-                  </a>.
+            <div infinite-scroll="" load-next="loadInfiniteScroll()" scroll-disabled="infiniteScrollControl.disabled" className="action-container">
+            <div>
+              <p className="message-header">
+                <a href="/agent/users/283601" target="_self" className="user-link">Joe Dirt</a> requested to
+                visit
+                <a href="#" className="property-link propertyClick" id="property_1224058">10 Las Palomas</a>
+              <span className="message-time">
+                <time dateTime="2015-09-26T00:44:26Z">3 days ago</time>
+              </span>
+              </p>
+
+              <div className="message-body">
+
+                <a href="#" className="showing-image property-link propertyClick" id="property_1224058">
+                  <img src="https://s3.amazonaws.com/realscout_v2/property_photos/property_photos/050/605/665/thumb/image_40717004_1.jpg?1443197913"/>
+                </a>
+
+                <p className="message-text">
+                  <a href="#" className="property-link propertyClick" id="property_1224058">10 Las Palomas</a>
+                  <span className="quote">"I'd like to see this property today. When are you available to show it?"</span>
                 </p>
               </div>
             </div>
-            <div className="col-sm-8">
-              <IOStore data={emulator.data_memory.io} />
-              <RegisterState registers={emulator.data_memory.registers} />
+
+            <div className="action-item-buttons">
+              <div>
+                <button data-ng-click="respond()"className="respond outline button">
+                  RESPOND
+                </button>
+                <button data-ng-click="complete()"className="complete button green with-icon outline">
+                  <i className="icon-circle-check action-item-icon"></i>
+                </button>
+                <button data-ng-click="dismiss()" className="dismiss button pink with-icon outline">
+                  <i className="icon-close action-item-icon"></i>
+                </button>
+              </div>
             </div>
+
+          </div>
           </div>
         }
       </Connector>
